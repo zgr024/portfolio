54649543d3bfc69e4c5841c5c307f7400fd8b0e0
Zachary Rosenberg <zgr024@gmail.com>
2013-11-07 16:59:42 -0500

    git

--- pages/default/default.php
+++ pages/default/default.php
@@ -2,6 +2,99 @@
 	$this->title = "Zachary Rosenberg, Complex Web Application & Mobile Developer";
 	$this->template('website','top');
 ?>
+	<section id="testimonials">
+        
+        <h2>Testimonials</h2>
+
+        <hr/>
+
+        <div class="has-floats">
+
+            <details class="testimonial">
+                <div class="photo">
+                    <img src="/images/people/sandy.jpg" alt="">
+                </div>
+                <div class="block">
+                    <div class="contact">
+                        <div class="name">
+                            Sandy Corsillo                </div>
+                        <a href="http://linkedin.com/pub/sandy-corsillo/a/560/46" class="linkedin" target="_blank"></a>
+                    </div>
+                    <div class="title">
+                        Owner and President            </div>
+                    <div class="statement">
+                        If he can handle our ERP integration, he can handle anything.            </div>
+                </div>
+            </details>    
+            <details class="testimonial">
+                <div class="photo">
+                    <img src="/images/people/zach.jpg" alt="">
+                </div>
+                <div class="block">
+                    <div class="contact">
+                        <div class="name">
+                            Zachary Rosenberg                </div>
+                        <a href="http://linkedin.com/in/zachrosenberg" class="linkedin" target="_blank"></a>
+                    </div>
+                    <div class="title">
+                        Lead Software Engineer            </div>
+                    <div class="statement">
+                        I've been working with Will for a decade.  The SkyPHP platform has been a lifesaver.            </div>
+                </div>
+            </details>
+            <details class="testimonial">
+                <div class="photo">
+                    <img src="/images/people/peter.jpg" alt="">
+                </div>
+                <div class="block">
+                    <div class="contact">
+                        <div class="name">
+                            Peter James Cryan                </div>
+                    </div>
+                    <div class="title">
+                        Founder and President            </div>
+                    <div class="statement">
+                        It's been a few years and I still get compliments on my website all the time.             </div>
+                </div>
+    		</details>
+            <details class="testimonial">
+                <div class="photo">
+                    <img src="/images/people/bauer.jpg" alt="">
+                </div>
+                <div class="block">
+                    <div class="contact">
+                        <div class="name">
+                            Michael Bauer                </div>
+                        <a href="http://linkedin.com/pub/michael-bauer/2/17a/120" class="linkedin" target="_blank"></a>
+                    </div>
+                    <div class="title">
+                        Executive Financial Officer            </div>
+                    <div class="statement">
+                        We don't call him "The Captain" for nothing. The Captain is the man!            </div>
+                </div>
+		    </details>
+            <details class="testimonial">
+                <div class="photo">
+                    <img src="/images/people/damian.jpg" alt="">
+                </div>
+                <div class="block">
+                    <div class="contact">
+                        <div class="name">
+                            Damian Sobrevilla                </div>
+                        <a href="http://linkedin.com/in/dsobrevilla" class="linkedin" target="_blank"></a>
+                    </div>
+                    <div class="title">
+                        Product Development Engineer            </div>
+                    <div class="statement">
+                        Gadget comes through for my start-up projects time and time again. Look no further.            </div>
+                </div>
+            </details>
+        </div>
+
+    </section>
+	<section id="projects">
+    	
+    </section>
 	<section id="contact">
 
         <h2>Contact Me</h2>
