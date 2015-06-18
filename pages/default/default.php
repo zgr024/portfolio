bb8fab9b2987aad166a8875f25a574c71cb862e1
Zachary Rosenberg <zgr024@gmail.com>
2015-06-18 13:31:03 -0400

    typekit

--- pages/default/default.php
+++ pages/default/default.php
@@ -12,15 +12,15 @@ $this->js[] = '/lib/jquery.cycle2.js';
 $this->js[] = '/lib/cycle2-ios6fix.js';
 $this->js[] = '/lib/stellar.js/jquery.stellar.min.js';
 $this->head[] = '
-    <script>
-		(function(d) {
-			var config = {
-				kitId: "tkq0wwy",
-				scriptTimeout: 3000
-			},
-			h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-		})(document);
-</script>
+    <script type="text/javascript">
+      (function() {
+        var config = {
+          kitId: "tkq0wwy",
+          scriptTimeout: 3000
+        };
+        var h=document.getElementsByTagName("html")[0];h.className+=" wf-loading";var t=setTimeout(function(){h.className=h.className.replace(/(\s|^)wf-loading(\s|$)/g," ");h.className+=" wf-inactive"},config.scriptTimeout);var tk=document.createElement("script"),d=false;tk.src="//use.typekit.net/"+config.kitId+".js";tk.type="text/javascript";tk.async="true";tk.onload=tk.onreadystatechange=function(){var a=this.readyState;if(d||a&&a!="complete"&&a!="loaded")return;d=true;clearTimeout(t);try{Typekit.load(config)}catch(b){}};var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(tk,s)
+      })();
+    </script>
 ';
 
 $this->title = 'Freelance Web Developer Tri-State Area 856-924-0869';
