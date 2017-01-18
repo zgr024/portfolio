592734bb108760718a48e418aa1b4f5fd4e17f7a
Zach Rosenberg <zgr024@gmail.com>
2017-01-18 12:14:30 -0500

    lennar test

---
+++ pages/lennar/lennar.php
@@ -0,0 +1,9 @@
+<?php
+
+$this->title = 'Lennar Prequalify Test';
+
+$this->template('html5', 'top');
+
+?>
+
+<iframe src="https://lasvegas.uamc.uamcstaging.com/lennar/prequalify?condo=Y&community=20023&homesite=123456789&scuid=ABCDEF-0123-2345-6789-GHIJKLMN&first=Zach&last=Rosenberg&email=zgr024@gmail.com&env=buymynewhomenowtest.lennar.com" style="width: 100%; height: 1000px;"></iframe>
